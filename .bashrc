6e2fe415450fb38c6805167bcbcc5f88c14156bb
Rinat Shigapov <rinatshigapov@gmail.com>
2012-01-10 18:01:55 +0600

    bash, git configs added, setup script polished

---
+++ .bashrc
@@ -0,0 +1,60 @@
+# DXist's .bashrc
+
+# Source global definitions
+if [ -r /etc/bashrc ]; then
+	. /etc/bashrc
+fi
+
+export PATH=$HOME/bin:$PATH:/usr/lib/git-core/
+export EDITOR=`which vim`
+
+# Git config
+export PS1='[\u@\h \W$(__git_ps1 " (%s)")]\$ '
+export GIT_PS1_SHOWDIRTYSTATE=true
+export GIT_PS1_SHOWSTASHSTATE=true
+
+if [ -r /etc/bash_completion.d/git ]; then
+	. /etc/bash_completion.d/git
+fi
+
+# aliases
+
+alias cp='cp -i'
+alias mv='mv -i'
+alias rm='rm -i'
+
+alias l='ls -lap'
+alias ls='ls -F --color'
+alias ll='ls -l'
+alias la='ls -lA'
+
+alias vi='vim'
+alias sim='sudo vim'
+
+# functions
+function del() {
+    if [ ! -d /tmp/.Trash ]; then
+        mkdir /tmp/.Trash
+    fi
+    mv -f "$1" /tmp/.Trash/
+}
+
+function ask() {
+	echo "$* [y/n]?"
+	read ans
+	if [ $ans = y -o $ans = Y -o $ans = yes -o $ans = Yes -o $ans = YES ]
+	then
+		return 0
+	fi
+
+	if [ $ans = n -o $ans = N -o $ans = no -o $ans = No -o $ans = NO ]
+	then
+		return 1
+	fi
+}
+
+
+# additional settings
+if [ -r ~/.bashrc.local ]; then
+	. ~/.bashrc.local
+fi
--- setup.sh
+++ setup.sh
@@ -11,6 +11,7 @@ DOTHOME=`pwd`
 LINK_MAP="
 gnome-terminal/keybindings:.gconf/apps/gnome-terminal/
 awesome:.config/
+bin:bin/
 "
 #
 # Git must be installed on your machine.
@@ -21,6 +22,11 @@ if ! type -p git &> /dev/null; then
 	exit 1
 fi
 
+compile() {
+	echo "*** Compiling extensions..."
+	cd $DOTHOME/.vim/bundle/command-t/ruby/command-t && ruby extconf.rb && make
+	cd $DOTHOME/.vim/bundle/vimproc && make -f make_gcc.mak
+}
 
 update() {
 	local github="https://raw.github.com"
@@ -28,24 +34,37 @@ update() {
 	echo "*** Updating..."
 
 	git submodule update --init \
-	&& git submodule foreach git pull origin master \
 	&& git submodule foreach --recursive git submodule update --init \
 	&& git submodule foreach --recursive git pull origin master
 
-	#read -p "Submodules was successfully updated. Are you sure you want to continue [y/n]? " ANSWER
-	#[[ $ANSWER == "n" || $ANSWER == "N" ]] && exit
+	compile
 
+	echo "*** Update finished"
+	if ask "Commit this update?"; then
+		git commit -a -m "Automated submodule update"
+	fi
+}
+
+checkdeps() {
+	echo "*** Checking dependecies"
+	if ! type -p ack &> /dev/null; then
+		echo "Can't find ack..."
+	fi
+	echo "Installing pep8..."
+	pip install pep8
 }
 
 install() {
 	update
-	# Create symbolic links
+	echo "*** Creating symbolic links"
 	for file in `ls -d .??* |egrep -v $DOTIGNORE`
 	do
 		if [ -e $HOME/$file -a ! -h $HOME/$file ]; then
+			echo "$file backed up to $HOME/_${file:1}"
 			mv $HOME/$file $HOME/_${file:1}
 		fi
-		ln -sf $DOTHOME/$file $HOME/$file
+		echo "Link for $file to $HOME/$file"
+		ln -sfT $DOTHOME/$file $HOME/$file
 	done
 
 	for line in $LINK_MAP
@@ -53,33 +72,20 @@ install() {
 		from=`echo $line|awk -F ":" '{ print $1 }'`
 		to=`echo $line|awk -F ":" '{ print $2 }'`
 		mkdir -p $HOME/$to
-		ln -sf $DOTHOME/$from $HOME/$to
+		echo "Link for $from to $HOME/$to"
+		ln -sfT $DOTHOME/$from $HOME/$to
 	done
 
+	checkdeps
 
-  # Compile command-t extension
-  echo "*** Compiling extensions..."
-  cd $DOTHOME/.vim/bundle/command-t && rake make
-  cd $DOTHOME/.vim/bundle/vimproc && make -f make_gcc.mak
-  echo "*** Installing pep8..."
-  pip install pep8
-
-
-  # Done
-  echo "*** Installed"
+	echo "*** Installed"
 }
 
-checkdeps() {
-	if ! type -p ack &> /dev/null; then
-		echo "*** Can't find ack..."
-	fi
-
-	echo "*** Done"
-}
 
 # Parse arguments
 case $1 in
 	update) update; exit ;;
 	checkdeps) checkdeps; exit ;;
+	compile) compile; exit ;;
 	install|*) install; exit ;;
 esac
