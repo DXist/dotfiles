1a8761f462c687e3b677eb2bb9cd2700466d0823
Rinat Shigapov <rinatshigapov@gmail.com>
2016-09-29 13:35:23 +0000

    sticky is not convinient for debugging

--- .pdbrc.py
+++ .pdbrc.py
@@ -7,6 +7,6 @@ from pdb import DefaultConfig
 
 class Config(DefaultConfig):
 
-    sticky_by_default = True
+    sticky_by_default = False
     # 40 + number - https://en.wikipedia.org/wiki/ANSI_escape_code#Colors
     current_line_color = 40
