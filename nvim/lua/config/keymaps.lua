82739d121c8a32c2e4cc0a5076df64bb6fbfea61
Rinat Shigapov <rinatshigapov@gmail.com>
2025-11-02 20:23:35 +0800

    try LazyVim

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.g.mapleader = ","
+
+-- save file
+-- vim.keymap.del({ "i", "x", "n", "s" }, "<ctrl-s>", { desc = "Save File" })
+vim.keymap.set({ "x", "n", "s" }, "<leader>s", "<cmd>w<cr><esc>", { desc = "Save File" })
